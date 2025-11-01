5ac49bdf84a935b2e44b0ac4cd9472bebe75cec5
radhagharge <163811565+radhagharge@users.noreply.github.com>
2025-11-01 19:33:55 +0530

    Change OpenAI API key in OpenAIModel
    Updated the API key for OpenAI integration.

--- models/openai_model.py
+++ models/openai_model.py
@@ -8,7 +8,7 @@ class OpenAIModel(ModelInterface):
     
 
     def __init__(self, model_name="gpt-4o-mini"):
-        self.client = OpenAI(api_key=os.getenv("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"))
+        self.client = OpenAI(api_key=os.getenv("sk-proj-xxxx")) #here,I used my created API key for integration purpose
         self.model_name = model_name
 
     def extract_entities(self, text: str, entities: list[str]):
@@ -30,3 +30,4 @@ class OpenAIModel(ModelInterface):
         tokens = response.usage.total_tokens if hasattr(response, "usage") else 0
         cost = tokens * 0.00001
         return content, latency, tokens, cost
+
